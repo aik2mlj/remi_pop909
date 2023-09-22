ceebbefc4a26ddd3d7e3ef0514ee44b973d4306f
aik2 <aik2mlj@gmail.com>
2023-09-22 23:58:46 +0400

    better inference

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from model import PopMusicTransformer
+from datetime import datetime
 import os
+import argparse
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--only-melody", action="store_true")
+    args = parser.parse_args()
+
+    chkpt_name = 'REMI-chord-melody' if args.only_melody else "REMI-chord"
+
     # declare model
     model = PopMusicTransformer(
-        checkpoint='REMI-tempo-checkpoint',
+        checkpoint=chkpt_name,
         is_training=False)
     
     # generate from scratch
@@ -13,14 +21,14 @@ def main():
         n_target_bar=16,
         temperature=1.2,
         topk=5,
-        output_path='./result/from_scratch.midi',
+        output_path=f"./result/gen({chkpt_name})_{datetime.now().strftime('%m-%d_%H%M')}.midi",
         prompt=None)
     
     # generate continuation
     model.generate(
         n_target_bar=16,
         temperature=1.2,
-        topk=5
+        topk=5,
         output_path='./result/continuation.midi',
         prompt='./data/evaluation/000.midi')
     
